e3b870e4eea42ae07ee0403d83d352a3a6f2b88d
Corey Moore <monorthwest@gmail.com>
2019-07-21 12:53:43 -0700

    ready for deployment

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const helmet = require('helmet');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -19,7 +20,6 @@ app.use(function validateBearerToken(req, res, next) {
   }
   const bearerToken = req.get('Authorization').split(' ')[1];
   const apiToken = process.env.API_TOKEN;
-  console.log('validate bearer token middleware');
 
   if (bearerToken !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
@@ -50,9 +50,19 @@ function handleGetPokemon(req, res) {
 app.get('/pokemon', handleGetPokemon);
 
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`
+  (`Server listening at http://localhost:${PORT}`
   );
 });
